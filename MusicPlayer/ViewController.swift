a7d96b086ca4b6313f572661e610c748f99f0725
gandeveloper <official.passawis@gmail.com>
2020-02-16 12:51:15 +0800

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Gan on 16/2/20.
+//  Copyright © 2020 passawis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
